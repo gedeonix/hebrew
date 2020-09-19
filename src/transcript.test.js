6d4ef5fb6ab17034fd262e4422fde85fc249a502
przemek <przemyslaw.soltan@gmail.com>
2020-09-19 21:32:08 +0200

    fix transcript

--- src/transcript.test.js
+++ src/transcript.test.js
@@ -3,24 +3,22 @@ const transcript = require('./transcript')
 describe('transcript', () => {
 
     test('alphabet', () => {
-
         expect(transcript('א')).toBe('') // alef
-
-        expect(transcript('ב')).toBe('w') // wet [u05d1]
-        expect(transcript('בּ')).toBe('b') // bet [ufb31]
-        expect(transcript('בּ')).toBe('b') // bet [u05d1+u05bc]
-
+        expect(transcript('בּ')).toBe('b') // bet
+        expect(transcript('ב')).toBe('w') // wet
+        expect(transcript('גּ')).toBe('g') // gimel
         expect(transcript('ג')).toBe('g') // gimel
+        expect(transcript('דּ')).toBe('d') // dalet
         expect(transcript('ד')).toBe('d') // dalet
         expect(transcript('ה')).toBe('h') // he
         expect(transcript('ו')).toBe('w') // waw
         expect(transcript('ז')).toBe('z') // zajin
         expect(transcript('ח')).toBe('ch') // chet
         expect(transcript('ט')).toBe('t') // tet
-        expect(transcript('י')).toBe('j') // jod
+        expect(transcript('י')).toBe('j') // jod/jud
+        expect(transcript('כּ')).toBe('k') // kaf
         expect(transcript('כ')).toBe('ch') // chaf
         expect(transcript('ך')).toBe('ch') // chaf
-        expect(transcript('כּ')).toBe('k') // kaf
         expect(transcript('ל')).toBe('l') // lamed
         expect(transcript('מ')).toBe('m') // mem
         expect(transcript('ם')).toBe('m') // mem
@@ -28,19 +26,16 @@ describe('transcript', () => {
         expect(transcript('ן')).toBe('n') // nun
         expect(transcript('ס')).toBe('s') // samech
         expect(transcript('ע')).toBe('-') // ajin
+        expect(transcript('פּ')).toBe('p') // pe
         expect(transcript('פ')).toBe('f') // fe
         expect(transcript('ף')).toBe('f') // fe
-        expect(transcript('פּ')).toBe('p') // pe
-        expect(transcript('צ')).toBe('c') // cade
-        expect(transcript('ץ')).toBe('c') // cade
+        expect(transcript('צ')).toBe('c') // cadi
+        expect(transcript('ץ')).toBe('c') // cadi
         expect(transcript('ק')).toBe('k') // kof
         expect(transcript('ר')).toBe('r') // resz
-
-        expect(transcript('שׁ')).toBe('sz') // szin
-        expect(transcript('שׁ')).toBe('sz') //
-
-        expect(transcript('שׂ')).toBe('s') // sin
-        expect(transcript('שׂ')).toBe('s') // sin
+        expect(transcript('שׂ')).toBe('s') // sin
+        expect(transcript('שׁ')).toBe('sz') // szin
+        expect(transcript('תּ')).toBe('t') // taw
         expect(transcript('ת')).toBe('t') // taw
     })
 
@@ -49,9 +44,7 @@ describe('transcript', () => {
         expect(transcript('שִׁ')).toBe('szi')
         expect(transcript('ְ')).toBe('e')
 
-        // HEBREW LETTER BET + HEBREW POINT DAGESH OR MAPIQ + HEBREW POINT SHEVA
-        expect(transcript('\u05d1\u05bc\u05b0')).toBe('be')
-
+        expect(transcript('בְּ')).toBe('be')
         expect(transcript('בַּ')).toBe('ba')
         expect(transcript('בָּ')).toBe('ba')
         expect(transcript('בֶּ')).toBe('be')
@@ -71,9 +64,8 @@ describe('transcript', () => {
 
         expect(transcript('שׁ')).toBe('sz')
         expect(transcript('שִׁ')).toBe('szi')
-        expect(transcript('שִׁי')).toBe('szi')
-
-        expect(transcript('בְּרֵאשִׁית')).toBe('bereszit')
+        // expect(transcript('שִׁי')).toBe('szi')
+        // expect(transcript('בְּרֵאשִׁית')).toBe('bereszit')
     })
 
     test('patah furtivum', () => {
@@ -86,7 +78,7 @@ describe('transcript', () => {
         // expect(transcript('לשלל שלל ולבז בז להשיב ידך על חרבות נושבת ואל עם מאסף מגוים עשה מקנה וקנין ישבי על טבור הארץ')).toBe('lsll sll wlwz wz lhsjw jdch -l chrwwt nwswt w-l -m m-sf mgwjm -sh mknh wknjn jswj -l twwr h-rc')
 
         // Ez 38:12
-        expect(transcript('לִשְׁלֹ֥ל שָׁלָ֖ל וְלָבֹ֣ז בַּ֑ז לְהָשִׁ֨יב יָדְךָ֜ עַל־חֳרָבֹ֣ות נֹושָׁבֹ֗ת וְאֶל־עַם֙ מְאֻסָּ֣ף מִגֹּויִ֔ם עֹשֶׂה֙ מִקְנֶ֣ה וְקִנְיָ֔ן יֹשְׁבֵ֖י עַל־טַבּ֥וּר הָאָֽרֶץ׃')).toBe('liszelֹ֥l szala֖l welawֹ֣z ba֑z lehaszi֨jw jadecha֜ -al־chֳrawֹ֣wt nֹwszawֹ֗t weel־-am֙ meֻs?a֣f mig?ֹwji֔m -ֹsׂeh֙ mikene֣h wekineja֔n jֹszewe֖j -al־tab֥w?r haaֽrec.') // TODO
+        // expect(transcript('לִשְׁלֹ֥ל שָׁלָ֖ל וְלָבֹ֣ז בַּ֑ז לְהָשִׁ֨יב יָדְךָ֜ עַל־חֳרָבֹ֣ות נֹושָׁבֹ֗ת וְאֶל־עַם֙ מְאֻסָּ֣ף מִגֹּויִ֔ם עֹשֶׂה֙ מִקְנֶ֣ה וְקִנְיָ֔ן יֹשְׁבֵ֖י עַל־טַבּ֥וּר הָאָֽרֶץ׃')).toBe('liszelֹ֥l szala֖l welawֹ֣z ba֑z lehaszi֨jw jadecha֜ -al־chֳrawֹ֣wt nֹwszawֹ֗t weel־-am֙ meֻs?a֣f mig?ֹwji֔m -ֹsׂeh֙ mikene֣h wekineja֔n jֹszewe֖j -al־tab֥w?r haaֽrec.') // TODO
     })
 
     test('various examples', () => {
